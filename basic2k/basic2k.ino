f121f4a12df7461f933727254187f4d880b475f8
thnikk <thnikk@github.com>
2017-10-19 03:32:54 -0700

    Added mouse buttons to remapper (basic 2k)

---
+++ basic2k/basic2k.ino
@@ -0,0 +1,339 @@
+/*************************************************
+  Basic/Basic+ Keypad
+    Written for Pro Micro/Leonardo. Features
+    1000hz polling rate and button remapper. Can
+    be upgraded to LED fairly easily by wiring
+    positive ends of the LEDs to pin 5 and 6.
+
+    As of 2/5/17, uses new remapper that allows
+    for more modifiers to be used. Also prevents
+    false-triggering making the keypad enter
+    the remapper by requiring an input of 0
+    before use.
+
+  Key Numbering
+    ---------
+  - | 1 | 2 |
+    ---------
+  ^ Side button is key 3
+  *Pinout is 2, 3, 4 for buttons 1, 2, and 3
+
+  For more info, please visit:
+  ----> http://thnikk.moe/
+  ----> https://www.etsy.com/shop/thnikk
+  ----> twitter.com/thnikk
+  ----> https://github.com/thnikk
+
+  Written by thnikk.
+*************************************************/
+
+#include <EEPROM.h>
+#include <Bounce2.h>
+#include <Keyboard.h>
+#include <Mouse.h>
+
+// Version number (increment to update EEPROM values)
+bool version = 0;
+char initMapping[] = {"zx"};
+
+// How many keys (0 indexed)
+const byte numkeys = 2;
+
+// Array for buttons (for use in for loop.)
+const byte button[] = { 2, 3, 4 };
+
+// Makes button press/release action happen only once
+bool pressed[2];
+
+// Mapping multidimensional array
+char mapping[2][3];
+
+// Arrays for modifier interpreter
+byte specialLength = 33; // Number of "special keys"
+String specialKeys[] = {
+  "shift", "ctrl", "super",
+  "alt", "f1", "f2", "f3",
+  "f4", "f5", "f6", "f7",
+  "f8", "f9", "f10", "f11",
+  "f12", "insert",
+  "delete", "backspace",
+  "enter", "home", "end",
+  "pgup", "pgdn", "up",
+  "down", "left", "right",
+  "tab", "escape", "MB1", 
+  "MB2", "MB3"
+};
+byte specialByte[] = {
+  129, 128, 131, 130,
+  194, 195, 196, 197,
+  198, 199, 200, 201,
+  202, 203, 204, 205,
+  209, 212, 178, 176,
+  210, 213, 211, 214,
+  218, 217, 216, 215,
+  179, 177, 1, 2, 3
+};
+
+byte inputBuffer; // Stores specialByte after conversion
+
+unsigned long previousMillis = 0;
+byte set = 0;
+
+// Bounce declaration
+Bounce * bounce = new Bounce[numkeys];
+
+void setup() {
+  Serial.begin(9600);
+  // Load + Initialize EEPROM
+  loadEEPROM();
+
+  // Set input pullup resistors
+  for (int x = 0; x <= numkeys; x++) {
+    pinMode(button[x], INPUT_PULLUP);
+    bounce[x].attach(button[x]);
+    bounce[x].interval(8);
+  }
+
+}
+
+void loadEEPROM() {
+  // Initialize EEPROM
+  if (EEPROM.read(0) != version) {
+    // Single values
+    EEPROM.write(0, version);
+    for (int x = 0; x < numkeys; x++) {
+      for (int  y= 0; y < 3; y++) {
+        if (y == 0) EEPROM.write(40+(x*3)+y, int(initMapping[x]));
+        if (y > 0) EEPROM.write(40+(x*3)+y, 0);
+      }
+    }
+  }
+  // Load values from EEPROM
+  for (int x = 0; x < numkeys; x++) for (int  y= 0; y < 3; y++) mapping[x][y] = char(EEPROM.read(40+(x*3)+y));
+}
+
+void loop() {
+
+  // Run to get latest bounce values
+  for(byte x=0; x<=numkeys; x++) bounce[x].update();
+
+  if ((millis() - previousMillis) > 1000) { // Check once a second to reduce overhead
+    if (Serial && set == 0) { // Run once when serial monitor is opened to avoid flooding the serial monitor
+      Serial.println("Please press 0 to enter the serial remapper.");
+      set = 1;
+    }
+    // If 0 is received at any point, enter the remapper.
+    if (Serial.available() > 0) if (Serial.read() == '0') remapSerial();
+    // If the serial monitor is closed, reset so it can prompt the user to press 0 again.
+    if (!Serial) set = 0;
+    previousMillis = millis();
+  }
+
+  keyboard();
+}
+
+
+// Remapper code
+// Allows keys to be remapped through the serial monitor
+void remapSerial() {
+  Serial.println("Welcome to the serial remapper!");
+  // Buffer variables (puting these at the root of the relevant scope to reduce memory overhead)
+  byte input = 0;
+
+  // Print current EEPROM values
+  Serial.print("Current values are: ");
+  for (int x = 0; x < numkeys; x++) {
+    for (int y = 0; y < 3; y++) {
+      byte mapCheck = int(mapping[x][y]);
+      if (mapCheck != 0){ // If not null...
+        // Print if regular character (prints as a char)
+        if (mapCheck > 33 && mapCheck < 126) Serial.print(mapping[x][y]);
+        // Otherwise, check it through the byte array and print the text version of the key.
+        else for (int z = 0; z < specialLength; z++) if (specialByte[z] == mapCheck){
+          Serial.print(specialKeys[z]);
+          Serial.print(" ");
+        }
+      }
+    }
+    // Print delineation
+    if (x < (numkeys - 1)) Serial.print(", ");
+  }
+  Serial.println();
+  // End of print
+
+  // Take serial inputs
+  Serial.println("Please input special keys first and then a printable character.");
+  Serial.println();
+  Serial.println("For special keys, please enter a colon and then the corresponding");
+  Serial.println("number (example: ctrl = ':1')");
+  // Print all special keys
+  byte lineLength = 0;
+
+  // Print table of special values
+  for (int y = 0; y < 67; y++) Serial.print("-");
+  Serial.println();
+  for (int x = 0; x < specialLength; x++) {
+    // Make every line wrap at 30 characters
+    byte spLength = specialKeys[x].length(); // save as variable within for loop for repeated use
+    lineLength += spLength + 6;
+    Serial.print(specialKeys[x]);
+    spLength = 9 - spLength;
+    for (spLength; spLength > 0; spLength--) { // Print a space
+      Serial.print(" ");
+      lineLength++;
+    }
+    if (x > 9) lineLength++;
+    Serial.print(" = ");
+    if (x <= 9) {
+      Serial.print(" ");
+      lineLength+=2;
+    }
+    Serial.print(x);
+    if (x != specialLength) Serial.print(" | ");
+    if (lineLength > 55) {
+      lineLength = 0;
+      Serial.println();
+    }
+  }
+  // Bottom line
+  if ((specialLength % 4) != 0) Serial.println(); // Add a new line if table doesn't go to end
+  for (int y = 0; y < 67; y++) Serial.print("-"); // Bottom line of table
+  Serial.println();
+  Serial.println("If you want two or fewer modifiers for a key and");
+  Serial.println("no printable characters, finish by entering 'xx'");
+  // End of table
+
+  for (int x = 0; x < numkeys; x++) { // Main for loop for each key
+
+    byte y = 0; // External loop counter for while loop
+    byte z = 0; // quickfix for bug causing wrong input slots to be saved
+    while (true) {
+      while(!Serial.available()){}
+      String serialInput = Serial.readString();
+      byte loopV = inputInterpreter(serialInput);
+
+      // If key isn't converted
+      if (loopV == 0){ // Save to array and EEPROM and quit; do and break
+        // If user finishes key
+        if (serialInput[0] == 'x' && serialInput[1] == 'x') { // Break if they use the safe word
+          for (y; y < 3; y++) { // Overwrite with null values (0 char = null)
+            EEPROM.write((40+(x*3)+y), 0);
+            mapping[x][y] = 0;
+          }
+          if (x < numkeys-1) Serial.print("(finished key,) ");
+          if (x == numkeys-1) Serial.print("(finished key)");
+          break;
+        }
+        // If user otherwise finishes inputs
+        Serial.print(serialInput); // Print once
+        if (x < 5) Serial.print(", ");
+        for (y; y < 3; y++) { // Normal write/finish
+          EEPROM.write((40+(x*3)+y), int(serialInput[y-z]));
+          mapping[x][y] = serialInput[y-z];
+        }
+        break;
+      }
+
+      // If key is converted
+      if (loopV == 1){ // save input buffer into slot and take another serial input; y++ and loop
+        EEPROM.write((40+(x*3)+y), inputBuffer);
+        mapping[x][y] = inputBuffer;
+        y++;
+        z++;
+      }
+
+      // If user input is invalid, print keys again.
+      if (loopV == 2){
+        for (int a = 0; a < x; a++) {
+          for (int d = 0; d < 3; d++) {
+            byte mapCheck = int(mapping[a][d]);
+            if (mapCheck != 0){ // If not null...
+              // Print if regular character (prints as a char)
+              if (mapCheck > 33 && mapCheck < 126) Serial.print(mapping[a][d]);
+              // Otherwise, check it through the byte array and print the text version of the key.
+              else for (int c = 0; c < specialLength; c++) if (specialByte[c] == mapCheck){
+                Serial.print(specialKeys[c]);
+                // Serial.print(" ");
+              }
+            }
+          }
+          // Print delineation
+          Serial.print(", ");
+        }
+        if (y > 0) { // Run through rest of current key if any inputs were already entered
+          for (int d = 0; d < y; d++) {
+            byte mapCheck = int(mapping[x][d]);
+            if (mapCheck != 0){ // If not null...
+              // Print if regular character (prints as a char)
+              if (mapCheck > 33 && mapCheck < 126) Serial.print(mapping[x][d]);
+              // Otherwise, check it through the byte array and print the text version of the key.
+              else for (int c = 0; c < specialLength; c++) if (specialByte[c] == mapCheck){
+                Serial.print(specialKeys[c]);
+                Serial.print(" ");
+              }
+            }
+          }
+        }
+
+      }
+    } // Mapping loop
+  } // Key for loop
+  Serial.println();
+  Serial.println("Mapping saved, exiting. To re-enter the remapper, please enter 0.");
+
+} // Remapper loop
+byte inputInterpreter(String input) { // Checks inputs for a preceding colon and converts said input to byte
+  if (input[0] == ':') { // Check if user input special characters
+    input.remove(0, 1); // Remove colon
+    int inputInt = input.toInt(); // Convert to integer
+    if (inputInt >= 0 && inputInt < specialLength) { // Checks to make sure length matches
+      inputBuffer = specialByte[inputInt];
+      Serial.print(specialKeys[inputInt]); // Print within function for easier access
+      Serial.print(" "); // Space for padding
+      return 1;
+    }
+    Serial.println();
+    Serial.println("Invalid code added, please try again.");
+    return 2;
+  }
+  else if (input[0] != ':' && input.length() > 3){
+    Serial.println();
+    Serial.println("Invalid, please try again.");
+    return 2;
+  }
+  else return 0;
+}
+
+// Does keyboard stuff
+void keyboard(){
+
+  if(!bounce[numkeys].read()) Keyboard.press(177);
+  if(bounce[numkeys].read()) Keyboard.release(177);
+
+  for (int a = 0; a < numkeys; a++) {
+    // Cycles through key and modifiers set
+    if (!pressed[a]) {
+      for (int b = 0; b < 3; b++) if (!bounce[a].read()) {
+        if (mapping[a][b] > 3) Keyboard.press(mapping[a][b]);
+        else {
+          if (mapping[a][b] == 1) Mouse.press(MOUSE_LEFT);
+          else if (mapping[a][b] == 2) Mouse.press(MOUSE_RIGHT);
+          else if (mapping[a][b] == 3) Mouse.press(MOUSE_MIDDLE);
+        }
+        pressed[a] = 1;
+      }
+    }
+    if (pressed[a]) {
+      for (int b = 0; b < 3; b++) if (bounce[a].read()) {
+        if (mapping[a][b] > 3) Keyboard.release(mapping[a][b]);
+        else {
+          if (mapping[a][b] == 1) Mouse.release(MOUSE_LEFT);
+          else if (mapping[a][b] == 2) Mouse.release(MOUSE_RIGHT);
+          else if (mapping[a][b] == 3) Mouse.release(MOUSE_MIDDLE);
+        }
+        pressed[a] = 0;
+      }
+    }
+
+  }
+}
